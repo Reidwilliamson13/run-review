8c86113952580f113fbeefebf9748538fc3ac931
ReidWilliamson <reidwilliamson2@gmail.com>
2022-02-02 15:27:06 -0500

    add review page

---
+++ react_spa/src/ReviewPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ReviewPage() {
+  return (
+    <div>
+      <h2>Share Your Experience!</h2>
+      <p>
+        The easiest thing to do is post on our{" "}
+        <a href="http://forum.kirupa.com">forums</a>.
+      </p>
+    </div>
+  );
+}
+
+export default ReviewPage;
