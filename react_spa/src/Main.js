ab46d349b3be8323920d17a34b268aedebb5b06a
ReidWilliamson <reidwilliamson2@gmail.com>
2022-02-02 15:13:38 -0500

    update main.js

---
+++ react_spa/src/Main.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div>
+      <h1>Simple SPA</h1>
+      <ul className="header">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/stuff">Stuff</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+      <div className="content"></div>
+    </div>
+  );
+}
+
+export default Main;
