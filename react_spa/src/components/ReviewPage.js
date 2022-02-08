021785455bd840e7cf6bc5b4d71cf69c08e0e762
ReidWilliamson <reidwilliamson2@gmail.com>
2022-02-07 21:03:58 -0500

    add backend

--- react_spa/src/components/ReviewPage.js
+++ react_spa/src/components/ReviewPage.js
@@ -1,25 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
+import Axios from "axios";
 
 function ReviewPage() {
+const url = ""
+const [data, setData] = useState({
+  name: "",
+  trail: "",
+  trailreview: ""
+})
+
+function submit(e){
+e.preventDefault();
+Axios.post(url,{
+  name: data.name,
+  trail: data.trail,
+  trailreview: data.trailreview
+})
+.then(res=>{
+  console.log(res.data)
+})
+}
+
+function handle(e){
+  const newdata={...data}
+  newdata[e.target.id] = e.target.value
+  setData(newdata)
+  console.log(newdata)
+}
   return (
     <div className="review_page">
       <h2>Share Your Experience!</h2>
       <p>Post your review!</p>
-      <form>
+      <form onSubmit={(e)=> submit(e)}>
         <div class="form-group">
           <div>
-            <input type="value" placeholder="Name"/>
+            <input onChange={(e)=>handle(e)} id="name" value={data.name} placeholder="Name" type="value"/>
           </div>
           <div>
-            <input type="value" placeholder="Trail/Park"/>
+            <input onChange={(e)=>handle(e)} id="trail" value={data.trail} placeholder="Trail/Park" type="value"/>
           </div>
           <label for="exampleFormControlTextarea1">Review</label>
           <textarea
+            onChange={(e)=>handle(e)} id="trailreview" value={data.trailreview}
+            placeholder="Write a review!"
             class="form-control"
-            id="exampleFormControlTextarea1"
             rows="3"
           ></textarea>
         </div>
+        <button>Submit</button>
       </form>
     </div>
   );
