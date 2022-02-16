cc93df412af6a71acafb455d80bb4d686d288a5d
ReidWilliamson <reidwilliamson2@gmail.com>
2022-02-16 13:45:15 -0500

    text update for user

--- react_spa/src/components/ReviewPage.js
+++ react_spa/src/components/ReviewPage.js
@@ -10,10 +10,10 @@ function ReviewPage() {
   function createCard(allReviews) {
     const reviewCard = allReviews.map((review) => {
       return (
-        <div className="card ">
+        <div className="card " >
           <div className="card-body">
-            <h5 className="card-title">{review.nameInput}</h5>
-            <h6 className="card-subtitle mb-2 text-muted">{review.trailInput}</h6>
+            <h5 className="card-title">{review.nameInput} says...</h5>
+            <h6 className="card-subtitle mb-2 text-muted">({review.trailInput})</h6>
             <p className="card-text">{review.reviewInput}</p>
           </div>
         </div>
