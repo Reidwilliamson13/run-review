7d4d6cd4c75001d501fa2252beed49f6303e8497
ReidWilliamson <reidwilliamson2@gmail.com>
2022-03-23 19:51:01 -0400

    props and bootstrap update home.js

--- react_spa/src/components/Home.js
+++ react_spa/src/components/Home.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
-    <div className="home_div">
-      <div>
-        <h2>HELLO</h2>
-        <p>
+    <div className="m-5 text-light w-75">
+      <div
+        className="p-4 rounded shadow-lg"
+        style={{ backgroundColor: "rgba(0,0,0,.1)" }}
+      >
+        <h2 className="display-2">HELLO</h2>
+        <p className="h5">
           This app is for running enthusiasts like myself who are interested in
           discovering all of Northern Virginia's beautiful parks and running
           trails. I will be posting a new park or trail every week with a
@@ -13,10 +19,13 @@ function Home() {
           trails, what to expect, level of difficulty, and safety information.
         </p>
         <br />
-        <p>
-          Feel free to share your experience with the community on the Reviews
-          tab!
-        </p>
+        <button
+          type="button"
+          className="btn btn-info text-light"
+          onClick={() => navigate("/reviewpage")}
+        >
+          SHARE ON THE REVIEWS TAB
+        </button>
       </div>
     </div>
   );
