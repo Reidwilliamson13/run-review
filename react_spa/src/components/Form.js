1d42aa48a1934de87f58707ea8e7c62eeaa7092a
ReidWilliamson <reidwilliamson2@gmail.com>
2022-03-23 19:49:31 -0400

    Bootstrap form.js update

--- react_spa/src/components/Form.js
+++ react_spa/src/components/Form.js
@@ -1,47 +1,62 @@
-import React from 'react'
+import React from "react";
 
-
-const Form = ({nameInput, trailInput, reviewInput, setName, setTrail, setReview, options, handleSubmit}) => {
-
-
+const Form = ({
+  nameInput,
+  trailInput,
+  reviewInput,
+  setName,
+  setTrail,
+  setReview,
+  options,
+  handleSubmit,
+}) => {
 
   return (
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <div>
-            <input
-              className="textspace_name"
-              value={nameInput}
-              id="name_input"
-              placeholder="Name"
-              type="text"
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div>
-            <select
-              className="textspace_trails"
-              value={trailInput}
-              onChange={(e) => setTrail(e.target.value)}
-            >
-                {options.map((o, idx) => <option key={idx}>{o}</option>)}
-            </select>
-          </div>
-          <label htmlFor="exampleFormControlTextarea1">Review</label>
-          <br />
-          <textarea
-            className="textspace_review"
-            id="trailreview_input"
-            placeholder="Write a review!"
-            rows="3"
-            type="text"
-            value={reviewInput}
-            onChange={(e) => setReview(e.target.value)}
-          ></textarea>
-        </div>
-        <button>Submit</button>
-      </form>
+    
+    <form
+      onSubmit={handleSubmit}
+      className="w-50 p-5 border border-secondary rounded"
+    >
+      <div className="form-group">
+        <label>Name</label>
+        <input
+          className="form-control"
+          value={nameInput}
+          placeholder="Name"
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label>Location</label>
+        <select
+          className="form-control"
+          value={trailInput}
+          onChange={(e) => setTrail(e.target.value)}
+        >
+          {options.map((o, idx) => (
+            <option key={idx}>{o}</option>
+          ))}
+        </select>
+      </div>
+      <div className="form-group">
+        <label>Review</label>
+        <textarea
+          className="form-control"
+          placeholder="Write a review!"
+          rows="3"
+          type="text"
+          value={reviewInput}
+          onChange={(e) => setReview(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit" className="btn btn-primary mt-4">
+        Submit
+      </button>
+    </form>
   );
-}
+};
 
-export default Form
+export default Form;
