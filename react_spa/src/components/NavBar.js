4ad0782d3125b3ee7782bac4364cc3256cad135e
ReidWilliamson <reidwilliamson2@gmail.com>
2022-03-23 20:02:34 -0400

    props and bootstrap update

--- react_spa/src/components/NavBar.js
+++ react_spa/src/components/NavBar.js
@@ -4,24 +4,24 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-      <div className="App">
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand href="#home">Run & Review App</Navbar.Brand>
-            <Nav className="me-auto">
-              <NavLink className="nav-link" to="/">
-                Home
-              </NavLink>
-              <NavLink className="nav-link" to="/runpage">
-                The Run
-              </NavLink>
-              <NavLink className="nav-link" to="/reviewpage">
-                Reviews
-              </NavLink>
-            </Nav>
-          </Container>
-        </Navbar>
-      </div>
+    <div className="App">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Run & Review App</Navbar.Brand>
+          <Nav className="me-auto">
+            <NavLink className="nav-link" to="/">
+              Home
+            </NavLink>
+            <NavLink className="nav-link" to="/runpage">
+              The Run
+            </NavLink>
+            <NavLink className="nav-link" to="/reviewpage">
+              Reviews
+            </NavLink>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
   );
 }
 
